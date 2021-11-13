c00433975d031c35f533535b50fcf18c7e627831
Dario Andrade Mendoza <darioandrade@email.arizona.edu>
2021-11-13 11:11:56 -0700

    auth changes

--- Middleware/check-auth.js
+++ Middleware/check-auth.js
@@ -10,6 +10,8 @@ const CheckAuth = (req, res, next) => {
   try {
     console.log(req.headers);
     const token = req.headers.authorization.split(" ")[1]; //look at app.js for expected headers => Authorization: 'Bearer TOKEN'
+    console.log(token);
+    console.log(!token);
     if (!token) {
       throw new Error("Authentication failed!");
     }
@@ -17,7 +19,7 @@ const CheckAuth = (req, res, next) => {
     const decodedToken = jwt.verify(token, process.env.JWT_KEY);
     console.log(decodedToken);
     req.userData = { userID: decodedToken.userID }; //can always add data to request object
-    console.log(req.userData);
+
     next();
   } catch (err) {
     const error = new HttpError("Autnentication failed!", 403);
